0b4aabb3d27aff55fbd8d2f11e0ac7d91587200f
Boris Grunwald <borisgrunwald@gmail.com>
2022-10-11 13:45:43 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todoId, setTodoId] = useState(1);
+  const [todo, setTodo] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function getNewTodo() {
+    setTodoId((todoId) => (todoId === 20 ? 1 : todoId + 1));
+  }
+
+  useEffect(() => {
+    async function fetchTodo() {
+      const url = `/.netlify/functions/todo?id=${todoId}`;
+      try {
+        setLoading(true);
+        const todo = await fetch(url).then((res) => res.json());
+        setTodo(todo.title);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchTodo();
+  }, [todoId]);
+
+  return (
+    <div className="App">
+      <p>
+        <button onClick={getNewTodo}> Get another todo </button>
+      </p>
+      <p>{loading ? "Loading..." : todo}</p>
+    </div>
+  );
+}
+
+export default App;
